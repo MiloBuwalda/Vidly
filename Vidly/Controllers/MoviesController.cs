eb1be48e890a2c567eaba8172cdc1d2f72d6f73e
MiloBuwalda <milo.buwalda@gmail.com>
2020-01-07 21:38:18 +0100

    update

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,8 +14,51 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            { 
+                Movie = movie,
+                Customers = customers
+            };
+            // ViewData["Movie"] = movie Don't use
+            // ViewBag -> Don't use
+
+            //ViewResult.ViewData.Model  <- Way to go
+
+
+
+            return View(viewModel);
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+ id);
+        }
+
+        public  ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        { 
+            return Content(year + "/" + month);
+        }
+
     }
 }
